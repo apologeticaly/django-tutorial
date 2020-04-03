bdc66815476e183683194192177262e5192bfdc8
Constantino <muheec@gmail.com>
2020-04-03 10:57:06 -0700

    hide

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/constantino/Documents/makeschool/bew-1.2/django-tutorial/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
